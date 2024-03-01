a987d210c27b8bed676c5353305e02296e3693dd
Ilya Shumeyko <ilya@shuma.ru>
2024-03-01 20:55:19 +0300

    Add storybook decorator (updateArgs)

--- src/components/VInput/ui/VInput.stories.js
+++ src/components/VInput/ui/VInput.stories.js
@@ -10,12 +10,17 @@ export default {
   },
 };
 
-const Template = (args) => ({
+const Template = (args, { updateArgs }) => ({
   components: { VInput },
   setup() {
     return { args };
   },
-  template: '<VInput v-bind="args" />',
+  template: '<VInput v-bind="args" @update:modelValue="updateArgs" />',
+  methods: {
+    updateArgs(modelValue) {
+      updateArgs({ ...args, modelValue })
+    },
+  },
 });
 
 export const Default = Template.bind({});

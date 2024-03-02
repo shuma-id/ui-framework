c2ffa814833b29af0c3cf6ca3d76b0d643edac97
Rodion-Gimranov <rodiongimranov6@gmail.com>
2024-03-02 13:17:03 +0300

    Code optimizations and improvements

--- src/components/VInput/ui/VInput.vue
+++ src/components/VInput/ui/VInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="form__container">
+  <div class="form__container" :class="{'__with-error': error, '__disabled': disabled}">
     <div class="input__wrapper">
       <input 
         type="text" id="VInput" placeholder=""
@@ -9,7 +9,6 @@
       <label for="VInput" class="placeholder" :class="{ 'error': error }">{{ placeholder }}</label>
       <img class="error-icon" src="./icon-errors.svg" :class="{ 'error': error }" alt="Error icon image">
     </div>
-    <span v-if="messageError" class="error-message" :class="{ 'messageError': messageError }">Error message</span>
   </div>
 </template>
 
@@ -17,10 +16,10 @@
 export default {
   name: 'VInput',
   props: {
+    modelValue: { type: String, default: ''},
     placeholder: {type: String, default: ''},
     disabled: {type: Boolean, default: false},
     error: {type: Boolean, default: false},
-    modelValue: { type: String, default: ''},
   },
   methods: {
     updateValue($event) {
@@ -41,6 +40,51 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
+
+  &.__with-error {
+    .input__wrapper {
+      .error {
+        color: #FF002B;
+        transition: 0.3s;
+      }
+
+      .input.error {
+        background: #FFF0F2;
+        color: #808080;
+        border: 1px solid transparent;
+
+        &:hover {
+          background: #FFF0F2;  
+          color: #808080;
+        }
+
+        &.__complete:not(:focus):hover {
+          background: #FFF0F2;
+          color: #808080;  
+        }
+      }
+
+      .input + .placeholder.error,
+      .input:focus + .placeholder.error {
+        color: #FF002B;
+        transition: 0.3s;
+      }
+    }
+  }
+
+  &.__disabled {
+    .input__wrapper {
+      .input:disabled {
+        background: #F7F7F7;
+        color: #808080;
+      }
+
+      .input:disabled:not(:focus):hover {
+        background: #F7F7F7;
+        color: #808080;
+      }
+    }
+  }
   
   .input__wrapper {
     position: relative;
@@ -56,7 +100,7 @@ export default {
       height: 64px;
 
       box-sizing: border-box; 
-      padding: 20px 39px 20px 18px;
+      padding: 20px 18px;
 
       background: #F7F7F7;
       border: 1px solid transparent;
@@ -82,10 +126,18 @@ export default {
         background: #F2F2F2;
         color: #000;  
       }
+    }
 
-      &:disabled {
-        background: #F7F7F7;
-        color: #808080;
+    .error-icon {
+      position: relative;
+      top: 0;
+      left: -33px;
+      
+      opacity: 0;
+      transition: 0.3s;
+
+      &.error {
+        opacity: 1;
       }
     }
 
@@ -102,52 +154,13 @@ export default {
       transition: 0.3s;
     }
 
-    .error-icon {
-      position: relative;
-      top: 0;
-      left: -33px;
-      width: 15px;
-      height: 15px;
-      opacity: 0;
-      transition: 0.3s;
-    }
-
-    .error-icon.error {
-      opacity: 1;
-    }
-
-    .error {
-      color: #FF002B;
-      transition: 0.3s;
-    }
-
-    .input.error{
-      background: #FFF0F2;
-      color: #808080;
-    }
-
     .input:focus + .placeholder,
     .input:not(:placeholder-shown) + .placeholder {
-      transform: translateY(8px);
+      transform: translate3D(0, 8px, 0);
       font-size: 12px;
       color: #A6A6A6;
       transition: 0.3s;
     }
-
-    .input + .placeholder.error,
-    .input:focus + .placeholder.error {
-      color: #FF002B;
-      transition: 0.3s;
-    }
-  }
-
-  .error-message {
-    margin-top: 6px;
-    margin-left: 18px;
-    font-size: 12px;
-    font-weight: 400;
-    color: #FF002B;
-    transition: 0.3s;
   }
 }
 </style>

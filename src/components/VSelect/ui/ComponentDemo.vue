3670d62487231fd3a40cdd09dc43665afb5cd6cb
Ilya Shumeyko <ilya@shuma.ru>
2024-04-26 19:02:59 +0300

    Add VBUtton to index.js

---
+++ src/components/VSelect/ui/ComponentDemo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ name }}</div>
+</template>
+
+<script>
+export default {
+    name: "ComponentDemo",
+    props: {
+        name: { type: String, default: "" },
+    },
+};
+</script>
+
+<style scoped lang="scss"></style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
+import VButton from "./VSelect/ui/VButton.vue";
 import VSelect from "./VSelect/ui/VSelect.vue";
 import VInput from "./VInput/ui/VInput.vue";
 import VDropdown from "./VDropdown/ui/VDropdown.vue";
 
-export { VSelect, VInput, VDropdown };
+export { VButton, VSelect, VInput, VDropdown };

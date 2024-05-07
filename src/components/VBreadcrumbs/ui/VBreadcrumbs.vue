73fa6c949876d0ff4063552b0b661f1988739265
SvetaPivovarova <svetlana.pivovarova@shuma.id>
2024-05-07 12:51:27 +0400

    Add storybook-vue3-router

--- src/components/VBreadcrumbs/ui/VBreadcrumbs.vue
+++ src/components/VBreadcrumbs/ui/VBreadcrumbs.vue
@@ -1,13 +1,18 @@
 <template>
-  <a></a>
-
+  <ul v-for="link in links">
+    <li>
+      <a :href="link">{{link}}</a>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "VBreadcrumbs",
   props: {
-
+    links: {
+      default: ['']
+    }
   }
 }
 </script>

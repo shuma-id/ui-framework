63b8d6f593ab45ef3715b904546cd2d9fcbc4d78
SvetaPivovarova <svetlana.pivovarova@shuma.id>
2024-05-14 17:39:17 +0400

    Add complete styles and plain validation

--- src/components/VOtp/ui/VOtp.vue
+++ src/components/VOtp/ui/VOtp.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="otp">
-        <div class="otp--mask">{{ displayCode }}</div>
+        <div class="otp--mask">
+            <div class="__item" v-for="item in mask">{{ item }}</div>
+        </div>
         <VInput
             id="OTP"
             type="text"
@@ -9,6 +11,9 @@
             :placeholder="text"
             @update:modelValue="otpCode = $event"
             :make-focused="makeFocused"
+            :max-length="maxLength"
+            :error="otpError"
+            :error-text="otpErrorText"
         ></VInput>
         <p class="otp__prompt">
             <a class="otp__link" href="#">Request again in 30 seconds</a> or
@@ -19,41 +24,50 @@
 
 <script>
 import VInput from "../../VInput/ui/VInput.vue";
+import { PATTERNS } from "../../../utils/constants.js";
 export default {
     name: "VOtp",
     components: { VInput },
     props: {},
     data() {
         return {
-            text: "Enter short code from sms",
-            mask: [],
+            text: "Enter short code from Email",
+            initialMask: new Array(6).fill("—"),
+            mask: new Array(6).fill("—"),
             otpCode: "",
             disabled: false,
             focused: true,
             makeFocused: true,
             maxLength: 6,
+            modelValue: "",
+            otpError: false,
+            otpErrorText: "",
         };
-    },
-    mounted() {
-        this.mask = new Array(6).fill("—");
     },
     watch: {
         otpCode() {
-            if (this.otpCode.length <= 6) {
-                this.mask = new Array(6).fill("—");
-                const arr = Array.from(this.otpCode.trim());
-                for (let i = 0; i < arr.length; i++) {
-                    this.mask[i] = arr[i];
-                }
+            this.otpError = false;
+            this.otpErrorText = "";
+            this.mask = this.initialMask;
+            const arr = Array.from(this.otpCode);
+            for (let i = 0; i < arr.length; i++) {
+                this.mask[i] = "";
             }
-            if (this.otpCode.length === 6) {
-                console.log("отправка кода на сервер");
+
+            if (this.otpCode.length === this.maxLength) {
+                if (this.isCodeValid) {
+                    this.otpError = false;
+                    this.otpErrorText = "";
+                } else {
+                    this.otpError = true;
+                    this.otpErrorText = "Short code can only contain numbers";
+                }
             }
         },
     },
     computed: {
-        displayCode() {
-            return this.mask.slice(-6).join(" ");
+        isCodeValid() {
+            return PATTERNS.CODE.test(this.otpCode);
         },
     },
 };
@@ -64,19 +78,21 @@ export default {
     position: relative;
     display: flex;
     flex-direction: column;
-    gap: 4px;
+    gap: 12px;
 
     &--mask {
         font-family: "SF Mono", monospace;
-        font-size: 16px;
-        line-height: 1.5;
         position: absolute;
-        top: 31px;
+        top: 29px;
         left: 18px;
-        z-index: 10;
-        height: 24px;
-        width: 130px;
-        background: var(--color-input-bg);
+        z-index: 1;
+        height: 2px;
+        display: flex;
+
+        .__item {
+            width: 18px;
+            pointer-events: none;
+        }
     }
 
     &__prompt {
@@ -97,6 +113,11 @@ export default {
         &:focus {
             color: #999999;
         }
+    }
+
+    &:deep .__field {
+        font-family: "SF Mono", monospace;
+        letter-spacing: 8px;
     }
 }
 </style>
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const PATTERNS = {
+    CODE: /[0-9]{6}/,
+};

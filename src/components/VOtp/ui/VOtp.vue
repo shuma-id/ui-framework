a6097085d6ca18a250c488619117d39fbc12ef0f
SvetaPivovarova <svetlana.pivovarova@shuma.id>
2024-05-14 13:00:36 +0400

    Add handle mask of OTP code

--- src/components/VOtp/ui/VOtp.vue
+++ src/components/VOtp/ui/VOtp.vue
@@ -1,12 +1,20 @@
 <template>
-    <VInput
-        id="OTP"
-        type="text"
-        :model-value="otpCode || mask"
-        :disabled="disabled"
-        type-input="input"
-        :placeholder="text"
-    ></VInput>
+    <div class="otp">
+        <div class="otp--mask">{{ displayCode }}</div>
+        <VInput
+            id="OTP"
+            type="text"
+            :model-value="otpCode"
+            :disabled="disabled"
+            :placeholder="text"
+            @update:modelValue="otpCode = $event"
+            :make-focused="makeFocused"
+        ></VInput>
+        <p class="otp__prompt">
+            <a class="otp__link" href="#">Request again in 30 seconds</a> or
+            <a class="otp__link" href="#">contact support.</a>
+        </p>
+    </div>
 </template>
 
 <script>
@@ -18,21 +26,77 @@ export default {
     data() {
         return {
             text: "Enter short code from sms",
-            mask: "— — —   — — —",
+            mask: [],
             otpCode: "",
             disabled: false,
             focused: true,
+            makeFocused: true,
+            maxLength: 6,
         };
     },
-    methods: {},
-    directives: {
-        focus: {
-            mounted(el) {
-                el.focus();
-            },
+    mounted() {
+        this.mask = new Array(6).fill("—");
+    },
+    watch: {
+        otpCode() {
+            if (this.otpCode.length <= 6) {
+                this.mask = new Array(6).fill("—");
+                const arr = Array.from(this.otpCode.trim());
+                for (let i = 0; i < arr.length; i++) {
+                    this.mask[i] = arr[i];
+                }
+            }
+            if (this.otpCode.length === 6) {
+                console.log("отправка кода на сервер");
+            }
+        },
+    },
+    computed: {
+        displayCode() {
+            return this.mask.slice(-6).join(" ");
         },
     },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.otp {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+
+    &--mask {
+        font-family: "SF Mono", monospace;
+        font-size: 16px;
+        line-height: 1.5;
+        position: absolute;
+        top: 31px;
+        left: 18px;
+        z-index: 10;
+        height: 24px;
+        width: 130px;
+        background: var(--color-input-bg);
+    }
+
+    &__prompt {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 24px;
+        color: #a6a6a6;
+    }
+
+    &__link {
+        color: #a6a6a6;
+        transition: color 0.3s;
+
+        &:hover {
+            color: #666666;
+        }
+
+        &:focus {
+            color: #999999;
+        }
+    }
+}
+</style>
